7e159b2908b9ee2b705afe36d6b9290a8f4413b8
Thinh Tran Xuan <you@example.com>
2021-06-09 12:14:16 +0700

    add partials and crud in first project with ruby on rails

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+  resources :pages do
+    collection do
+      get :about
+    end
+  end
+  root 'pages#home'
+end
